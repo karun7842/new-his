ac0bcf3b8e9f410a68d68a7334f3a2a1d0f03766
karun7842 <70625725+karun7842@users.noreply.github.com>
2024-12-14 22:34:50 +0530

    Update AppointmentScheduling.java

--- AppointmentSchedulingSystem/src/main/java/com/his/AppointmentSchedulingSystem/AppointmentScheduling.java
+++ AppointmentSchedulingSystem/src/main/java/com/his/AppointmentSchedulingSystem/AppointmentScheduling.java
@@ -3,11 +3,29 @@ package com.his.AppointmentSchedulingSystem;
 import com.his.AppointmentSchedulingSystem.controller.AppointmentController;
 import com.his.AppointmentSchedulingSystem.view.AppointmentView;
 
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
 public class AppointmentScheduling {
     public static void main(String[] args) {
-        AppointmentView view = new AppointmentView();
-        AppointmentController controller = new  AppointmentController(view);
-        controller.viewAppointments();
-        view.display();
+        SwingUtilities.invokeLater(() -> {
+            try {
+                // Set Nimbus look and feel
+                for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                    if ("Nimbus".equals(info.getName())) {
+                        UIManager.setLookAndFeel(info.getClassName());
+                        break;
+                    }
+                }
+
+                AppointmentView view = new AppointmentView();
+                AppointmentController controller = new AppointmentController(view);
+                controller.viewAppointments();
+                view.display();
+            } catch (Exception e) {
+                System.err.println("Failed to launch the application: " + e.getMessage());
+                e.printStackTrace();
+            }
+        });
     }
 }
