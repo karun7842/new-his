b100a4650e891efc2e67cba13fc35d75f725fa17
karun7842 <70625725+karun7842@users.noreply.github.com>
2024-12-14 23:32:27 +0530

    Update AppointmentScheduling.java

--- AppointmentSchedulingSystem/src/main/java/com/his/AppointmentSchedulingSystem/AppointmentScheduling.java
+++ AppointmentSchedulingSystem/src/main/java/com/his/AppointmentSchedulingSystem/AppointmentScheduling.java
@@ -3,11 +3,28 @@ package com.his.AppointmentSchedulingSystem;
 import com.his.AppointmentSchedulingSystem.controller.AppointmentController;
 import com.his.AppointmentSchedulingSystem.view.AppointmentView;
 
+import javax.swing.*;
+
 public class AppointmentScheduling {
     public static void main(String[] args) {
-        AppointmentView view = new AppointmentView();
-        AppointmentController controller = new  AppointmentController(view);
-        controller.viewAppointments();
-        view.display();
+        // Set Nimbus Look and Feel
+        try {
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            System.err.println("Nimbus Look and Feel not applied. Falling back to default.");
+        }
+
+        // Launch Application
+        SwingUtilities.invokeLater(() -> {
+            AppointmentView view = new AppointmentView();
+            AppointmentController controller = new AppointmentController(view);
+            controller.viewAppointments();
+            view.display();
+        });
     }
 }
